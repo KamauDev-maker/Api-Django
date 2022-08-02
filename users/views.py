494b6c317f6a254845c9ff84cfd187dd0bd9ca44
KamauDev-maker <oscarnjenga@gmail.com>
2022-08-02 14:34:19 +0300

    testing the login view

--- users/views.py
+++ users/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+from .models import User
+
 
 
 # Create your views here.
@@ -13,4 +16,21 @@ class RegisterView(APIView):
         serializer.save()
         return Response(serializer.data)
         
-        
+class LoginView(APIView):
+    def post(self, request):
+       email = request.data['email']
+       password = request.data['password']
+       
+       user = User.objects.filter(email=email).first()
+       
+       if user is None:
+           raise AuthenticationFailed('user not found')
+       
+       if not user.check_password(password):
+           raise AuthenticationFailed('password is incorrect')  
+       
+       return Response( {
+           'message':'login successfully'
+        })
+          
+    
