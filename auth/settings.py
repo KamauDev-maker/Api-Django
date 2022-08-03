f599408fed11dc6301cc58ec73e75039da33e6f8
KamauDev-maker <oscarnjenga@gmail.com>
2022-08-03 13:44:10 +0300

    creating a logout route

--- auth/settings.py
+++ auth/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'users',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,3 +128,7 @@ AUTH_USER_MODEL = 'users.User'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True #FOR COOKIES
--- users/views.py
+++ users/views.py
@@ -4,8 +4,7 @@ from .serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from .models import User
-
-
+import jwt, datetime
 
 # Create your views here.
 
@@ -28,9 +27,46 @@ class LoginView(APIView):
        
        if not user.check_password(password):
            raise AuthenticationFailed('password is incorrect')  
+        
+       payload = {
+            'id': str(user.id),
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
        
-       return Response( {
-           'message':'login successfully'
-        })
-          
+       token = jwt.encode(payload, 'secret', algorithm='HS256')
+       
+       response = Response()
+       
+       response.set_cookie(key='jwt', value=token, httponly=True)
+       response.data = {
+           'jwt': token
+       }
+       return response
+   
+class UserView(APIView):
+
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
+            'message': 'logout successfully'
+        }
+        return response   
     
